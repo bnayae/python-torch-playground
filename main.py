2635ede2c032d4cd3efae630d40aa93a752f30b0
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-03-23 11:41:25 +0200

    build: dependencies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://code.visualstudio.com/docs/python/python-tutorial
 # https://wiki.python.org/moin/BeginnersGuide/Programmers
 # https://python.land/python-tutorial
+# PyTorch: https://www.youtube.com/watch?v=pcX4a5xZsIQ&t=97s
 
 
 # Setup command
@@ -8,6 +9,9 @@
 
 import sys
 
+import torch
+
 msg = "hello python"
 print(msg)
 print(f"Hi, Python version = {sys.version}")
+print(f"Random = {torch.rand(1).item()}")
